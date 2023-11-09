ff404c70ea9fb9b99d6d99b04541ee2c9d66cd84
Mwambui123 <maurinawambuia@gmail.com>
2023-11-09 10:44:55 +0300

    Completed Assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -14,9 +14,21 @@ test("renders the correct child components", () => {
     Uncomment the line below to see the DOM elements being returned
     by the App component in your terminal when you run the tests
   */
-  // screen.debug();
+  //screen.debug();
 
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
   expect(container.querySelector("#about")).toBeInTheDocument();
 });
+
+function Hello(props) {
+  return <link><h2> <a href="I'm a link"></a>{props.about}</h2></link>
+}
+
+function App(){
+  return(
+    <div>
+      <a href="I'm a link"></a>
+    </div>
+  )
+}
